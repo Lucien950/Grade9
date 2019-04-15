c1706fe3351642b63fd7dfac55d57bcd26869a27
Edwino <jessicawu414@gmail.com>
2019-04-15 15:10:02 -0400

    Progress bar changes

--- Personal Website/Template/template.js
+++ Personal Website/Template/template.js
@@ -2,18 +2,6 @@ window.addEventListener("scroll", event =>{
   let scrollTop = document.documentElement.scrollTop;
   let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let progress = 100 * (scrollTop/scrollHeight)
-  document.querySelector("#progress").style.width = progress+"%";
-});
-import barba from '@barba/core';
-import barbaCss from '@barba/css';
-
-// tells barba to use the css module
-barba.use(barbaCss);
-
-barba.init({
-  transitions: [
-    {
-      name: 'my-transition',
-    },
-  ],
+  document.querySelector(".progress-bar").style.width = progress+"%";
+  document.querySelector(".progress-bar").setAttribute('aria-valuenow', progress);
 });
