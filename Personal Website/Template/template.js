08e18d19ff7e8b659c5fc42cf5d97aea8260625f
Edwino <jessicawu414@gmail.com>
2019-04-17 13:52:49 -0400

    Learning Progress

--- Personal Website/Template/template.js
+++ Personal Website/Template/template.js
@@ -2,6 +2,6 @@ window.addEventListener("scroll", event =>{
   let scrollTop = document.documentElement.scrollTop;
   let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let progress = 100 * (scrollTop/scrollHeight)
-  document.querySelector(".progress-bar").style.width = progress+"%";
-  document.querySelector(".progress-bar").setAttribute('aria-valuenow', progress);
+  document.querySelector(".scroll-progress").style.width = progress+"%";
+  document.querySelector(".scroll-progress").setAttribute('aria-valuenow', progress);
 });
