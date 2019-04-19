c59b56a9e368ffa9a2513e41277f69486899623d
Edwino <jessicawu414@gmail.com>
2019-04-19 18:05:14 -0400

    quick mats
    Added Music Page
    
    about page overhaul
    
    progressbar overhaul

--- Personal Website/music/template.js
+++ Personal Website/music/template.js
@@ -2,6 +2,6 @@ window.addEventListener("scroll", event =>{
   let scrollTop = document.documentElement.scrollTop;
   let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let progress = 100 * (scrollTop/scrollHeight)
-  document.querySelector(".scroll-progress div").style.width = progress+"%";
-  document.querySelector(".scroll-progress div").setAttribute('aria-valuenow', progress);
+  document.querySelector(".scroll-progress").style.width = progress+"%";
+  document.querySelector(".scroll-progress").setAttribute('aria-valuenow', progress);
 });
